2143061a286cce77e7ab577bdbde036345f86321
sbx_user1051 <sbx_user1051@169.254.80.1>
2025-12-15 17:58:58 +0000

    Mon Dec 15 17:58:58 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -6,15 +6,21 @@ import java.util.HashMap;
 public class CheckoutSolution {
     public Integer checkout(String skus) {
         HashMap<String, Integer> basket = new HashMap<String, Integer>();
+        String[] items = {"A", "B", "C", "D"};
+        Integer[] prices = {50, 30, 20, 15};
         for (String sku : skus.split(",")) {
             if (!basket.containsKey(sku)) basket.put(sku, 0);
             else basket.put(sku, basket.get(sku) + 1);
         }
-        
+
+        for () {
+
+        }
         System.out.println(skus);
 
 
         return pr
     }
 }
+
 
