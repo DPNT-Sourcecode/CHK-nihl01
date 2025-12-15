5ea37c6a2ea0546b6d77fabad37f3e258140c415
sbx_user1051 <sbx_user1051@169.254.80.1>
2025-12-15 18:01:58 +0000

    Mon Dec 15 18:01:58 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -8,19 +8,30 @@ public class CheckoutSolution {
         HashMap<String, Integer> basket = new HashMap<String, Integer>();
         String[] items = {"A", "B", "C", "D"};
         Integer[] prices = {50, 30, 20, 15};
+        Integer total = 0;
+
         for (String sku : skus.split(",")) {
             if (!basket.containsKey(sku)) basket.put(sku, 0);
             else basket.put(sku, basket.get(sku) + 1);
         }
 
-        for () {
+        for (int i = 0; i < items.length; i++){
+            if (items[i].equals("A")) {
+                total += 
+            }
+            else if (items[i].equals("B")) {
 
+            }
+            else {
+
+            }
         }
         System.out.println(skus);
 
 
-        return pr
+        return total;
     }
 }
+
 
 
