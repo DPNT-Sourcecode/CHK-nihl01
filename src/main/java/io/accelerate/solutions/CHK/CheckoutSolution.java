e774301fb205eda4a4ac77c600acc8480dc360ac
sbx_user1051 <sbx_user1051@169.254.80.1>
2025-12-15 17:55:58 +0000

    Mon Dec 15 17:55:58 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -1,9 +1,20 @@
 package io.accelerate.solutions.CHK;
 
 import io.accelerate.runner.SolutionNotImplementedException;
+import java.util.HashMap;
 
 public class CheckoutSolution {
     public Integer checkout(String skus) {
-        throw new SolutionNotImplementedException();
+        HashMap<String, Integer> basket = new HashMap<String, Integer>();
+        for (String sku : skus.split(",")) {
+            if (!basket.containsKey(sku)) basket.put(sku, 0);
+            else basket.put(sku, basket.get(sku) + 1);
+        }
+        
+        System.out.println(skus);
+
+
+        return pr
     }
 }
+
