d973e477fe60d4847063eab2fc8caf4390e948bc
sbx_user1051 <sbx_user1051@169.254.80.1>
2025-12-15 20:09:16 +0000

    Mon Dec 15 20:09:16 UTC 2025

--- src/test/java/io/accelerate/solutions/CheckoutSolutionTest.java
+++ src/test/java/io/accelerate/solutions/CheckoutSolutionTest.java
@@ -1,4 +1,25 @@
 package io.accelerate.solutions;
 
+import io.accelerate.solutions.CHK.CheckoutSolution;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class CheckoutSolutionTest {
+    private CheckoutSolution checkout;
+
+    @BeforeEach
+    public void setUp() {
+        checkout = new CheckoutSolution();
+    }
+
+    @Test
+    public void compute_sum() {
+        System.out.println(checkout.checkout("ABBEE"));
+        assertThat(checkout.checkout("ABBEE").equals(160), equalTo(true));
+    }
 }
+
